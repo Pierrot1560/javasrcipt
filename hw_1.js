620f6947995e339c988b053325556f694e4697a0
Pierrot1560 <ufyfuof7575@gmail.com>
2026-07-22 12:45:33 +0300

    lesson #16

--- hw_1.js
+++ hw_1.js
@@ -1,1 +1,55 @@
-console.log('Hello World!')
+'use strict'
+// 1 задание
+// let x = 20
+// let y = 58
+// let z = 42
+// console.log(x + y + z);
+
+// 2 задание
+// const sec = 1
+// let secInMin = sec * 60
+// console.log(secInMin);
+// const min = 1
+// let minInHour = min * 60
+// console.log(minInHour);
+// let hour = 1
+// let hourInDay = hour * 24
+// console.log(hourInDay);
+// let hourInYear = hourInDay * 365
+// console.log(hourInYear);
+// let myAgeInSeconds =21 * hourInYear * minInHour * secInMin
+// console.log (myAgeInSeconds);
+
+// 3 задание
+
+// let count = 42
+// let userName = '42'
+// console.log(String(count));
+// console.log(Number(userName));
+// console.log (count + '');
+// console.log (userName - 0);
+
+
+// 4 задание
+// let a = 1
+// let b = 2
+// let c = 'белых медведей'
+// console.log(1 +  (b  +  c));
+
+// 5 задание(не сделано)
+//  let w1 = 'доступ'
+//  let w2 = 'морпех'
+//  let w3 = 'наледь'
+//  let w4 = 'попрек'
+//  let w5 = 'рубило'
+//  let words = ['доступ','рубило','морпех','наледь','попрек']
+//  let lengthWords = len(words)
+//  console.log (lengthWords);
+
+// 6 задание (не сделано) 
+
+// 7 задание
+let userNamePromt = prompt('Ваше имя?')
+let userAgePromt = +prompt('Ваш возраст?')
+console.log(userNamePromt);
+console.log(userAgePromt);
