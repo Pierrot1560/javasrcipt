9f3cfe8ebb0991d9bd3ea051424ab12bb4155330
Pierrot1560 <ufyfuof7575@gmail.com>
2026-07-23 14:35:12 +0300

    lesson #16(mistakes corretced)

--- hw_1.js
+++ hw_1.js
@@ -1,29 +1,29 @@
 'use strict'
 // 1 задание
-// let x = 20
-// let y = 58
-// let z = 42
+// const x = 20
+// const y = 58
+// const z = 42
 // console.log(x + y + z);
 
 // 2 задание
 // const sec = 1
-// let secInMin = sec * 60
+// const secInMin = 60
 // console.log(secInMin);
 // const min = 1
-// let minInHour = min * 60
+// const minInHour = 60
 // console.log(minInHour);
-// let hour = 1
-// let hourInDay = hour * 24
+// const hour = 1
+// const hourInDay = 24
 // console.log(hourInDay);
-// let hourInYear = hourInDay * 365
+// const hourInYear = hourInDay * 365
 // console.log(hourInYear);
-// let myAgeInSeconds =21 * hourInYear * minInHour * secInMin
+// const myAgeInSeconds =21 * hourInYear * minInHour * secInMin
 // console.log (myAgeInSeconds);
 
 // 3 задание
 
-// let count = 42
-// let userName = '42'
+// const count = 42
+// const userName = '42'
 // console.log(String(count));
 // console.log(Number(userName));
 // console.log (count + '');
@@ -31,25 +31,29 @@
 
 
 // 4 задание
-// let a = 1
-// let b = 2
-// let c = 'белых медведей'
+// const a = 1
+// const b = 2
+// const c = 'белых медведей'
 // console.log(1 +  (b  +  c));
 
-// 5 задание(не сделано)
-//  let w1 = 'доступ'
-//  let w2 = 'морпех'
-//  let w3 = 'наледь'
-//  let w4 = 'попрек'
-//  let w5 = 'рубило'
-//  let words = ['доступ','рубило','морпех','наледь','попрек']
-//  let lengthWords = len(words)
+// 5 задание 
+//  const w1 = 'доступ'
+//  const w2 = 'морпех'
+//  const w3 = 'наледь'
+//  const w4 = 'попрек'
+//  const w5 = 'рубило'
+//  const lengthWords = (w1 + w2 + w3 + w4 + w5).length
 //  console.log (lengthWords);
 
 // 6 задание (не сделано) 
 
+const var1 = true
+const var2 = 'true'
+console.log (`Variable: ${var1} have type: ${typeof var1}`)
+console.log (`Variable: ${var2} have type: ${typeof var2}`)
+
 // 7 задание
-let userNamePromt = prompt('Ваше имя?')
-let userAgePromt = +prompt('Ваш возраст?')
-console.log(userNamePromt);
-console.log(userAgePromt);
+// const userNamePromt = prompt('Ваше имя?')
+// const userAgePromt = +prompt('Ваш возраст?')
+// console.log(userNamePromt);
+// console.log(userAgePromt);
