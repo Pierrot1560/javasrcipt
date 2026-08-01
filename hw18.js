d563dc486fbba24e56388dafc080af09da0e3e6d
Pierrot1560 <ufyfuof7575@gmail.com>
2026-08-01 19:44:12 +0300

    mistakes corrected

--- hw18.js
+++ hw18.js
@@ -25,14 +25,30 @@
 
 // 3 задание
 // function trimString (str, from, to) {
-//   return str.slice(from, to);
+// let result = '';
+// for (let i = from; i < to; i++) {
+    
+//     if (i >= 0 && i < str.length) {
+//       result += str[i]; 
+//     }
+//   }
+
+//   return result;
 // }
-// console.log (trimString ('Hello world', 0 , 4));
+// console.log(trimString('Hello world ?',1,3))
 
 // 4 задание
-// function getSumNumbers(number){
+function getSumNumbers(number) {
+  let sum = 0;
+  let str = number + ''; 
+  for (let i = 0; i < str.length; i++) {
+    sum += +str[i]; 
+  }
+
+  return sum
     
-// }
+}
+console.log(getSumNumbers(2004))
 
 // 5 задание
 // function getSum(a, b) {
@@ -49,22 +65,22 @@
 // console.log(getSum(1,2))
 
 // 6 задание
-function foo() {
-  console.log(foo.name);
-}
+// function foo() {
+//   console.log(foo.name);
+// }
 
 
-function boo() {
-  console.log(boo.name);
-}
+// function boo() {
+//   console.log(boo.name);
+// }
 
 
-function fooBoo(flag) {
-  if (flag) {
-    foo(); 
-  } else {
-    boo(); 
-  }
-}
-fooBoo(true);  
-fooBoo(false); 
+// function fooBoo(flag) {
+//   if (flag) {
+//     foo(); 
+//   } else {
+//     boo(); 
+//   }
+// }
+// fooBoo(true);  
+// fooBoo(false); 
